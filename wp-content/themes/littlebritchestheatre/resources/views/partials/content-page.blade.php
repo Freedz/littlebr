d9e789898c245ddf62ea78b75302294474db65b2
Godofredo Padagas Jr <fred@cirrena.com>
2021-06-14 23:51:16 +0800

    contact us pages and adjustments

--- wp-content/themes/littlebritchestheatre/resources/views/partials/content-page.blade.php
+++ wp-content/themes/littlebritchestheatre/resources/views/partials/content-page.blade.php
@@ -1,2 +1,2 @@
-<div class="content-area col-md-4">@php the_content() @endphp <div style="clear:both"></div></div>
+<div class="content-area col-md-8 col-xl-6">@php the_content() @endphp <div style="clear:both"></div></div>
 {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
--- wp-content/uploads/cache/aee2ff98b27ba3c5e692e0c1102f0c1095c864f9.php
+++ wp-content/uploads/cache/aee2ff98b27ba3c5e692e0c1102f0c1095c864f9.php
@@ -1,3 +1,3 @@
-<div class="content-area col-md-4"><?php the_content() ?> <div style="clear:both"></div></div>
+<div class="content-area col-md-8 col-xl-6"><?php the_content() ?> <div style="clear:both"></div></div>
 <?php echo wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 
